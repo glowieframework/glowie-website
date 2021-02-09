7a98ab86827946be0eb477bdb5072c506e81d481
Glowie Framework <darksunstudiogyn@gmail.com>
2021-02-09 14:54:32 -0300

    base href update

--- includes/head.php
+++ includes/head.php
@@ -1,4 +1,4 @@
-<base href="/glowie-website/">
+<base href="/">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
 <link rel="stylesheet" href="assets/css/style.css">
